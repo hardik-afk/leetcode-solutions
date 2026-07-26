43977a181846850528d47f74d98c25c94266737a
hardik-afk <hardiksharma31082005@gmail.com>
2026-07-26 14:33:15 +0530

    Sync LeetCode solution: Maximum Absolute Sum of Any Subarray (Java)

--- Medium/1849_Maximum_Absolute_Sum_of_Any_Subarray.java
+++ Medium/1849_Maximum_Absolute_Sum_of_Any_Subarray.java
@@ -6,27 +6,29 @@
  * Runtime: 7 ms
  * Memory: 66.7 MB
  * Synced From: LeetCode
- * Date: 2026-07-16
+ * Date: 2026-07-26
  */
 
 class Solution {
-    public int maxAbsoluteSum(int[] nums) {
+    public int maxAbsoluteSum(int[] nums) { // maxm sum subarrya and minm susm subarry nikal ke 
+                                            // minm ka absoule krlenge or fir dono ka maxm nikal
+        int n = nums.length;                // lenge.
 
-        int maxEnding = nums[0];
+        int best_end = nums[0];
         int maxSum = nums[0];
 
-        int minEnding = nums[0];
+        int worst_end = nums[0];
         int minSum = nums[0];
 
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 1; i < n; i++) {
 
-            // Maximum subarray (Kadane)
-            maxEnding = Math.max(nums[i], maxEnding + nums[i]);
-            maxSum = Math.max(maxSum, maxEnding);
+            // Maximum Sum nikal rahe hai  Kadane se
+            best_end = Math.max(best_end + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, best_end);
 
-            // Minimum subarray (Reverse Kadane)
-            minEnding = Math.min(nums[i], minEnding + nums[i]);
-            minSum = Math.min(minSum, minEnding);
+            // Minimum Sum nikal rahe hai  Kadane se
+            worst_end = Math.min(worst_end + nums[i], nums[i]);
+            minSum = Math.min(minSum, worst_end);
         }
 
         return Math.max(maxSum, Math.abs(minSum));
