419c0584fd9c55f9ab10674de30f061d4a3907e4
hardik-afk <hardiksharma31082005@gmail.com>
2026-07-26 17:52:00 +0530

    Sync LeetCode solution: Maximum Sum Circular Subarray (Java)

--- Medium/0954_Maximum_Sum_Circular_Subarray.java
+++ Medium/0954_Maximum_Sum_Circular_Subarray.java
@@ -3,37 +3,47 @@
  * Problem ID: 954
  * Difficulty: Medium
  * Language: Java
- * Runtime: 5 ms
- * Memory: 51 MB
+ * Runtime: 11 ms
+ * Memory: 51.4 MB
  * Synced From: LeetCode
- * Date: 2026-07-17
+ * Date: 2026-07-26
  */
 
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
+
+        int n = nums.length;
+        int best_end = nums[0];
+        int worst_end = nums[0];
+        int max_sum = nums[0];
+        int min_sum = nums[0];
+        int res;
+
+        for( int i = 1 ; i < n; i++ ){
+           // max sum subarray nikal rahe hai yha pr
+            best_end = Math.max( best_end + nums[i] , nums[i]);
+            max_sum = Math.max( best_end, max_sum);
+
+            // ab yha min sum subarray nikal rahe hai
+            worst_end = Math.min(worst_end + nums[i] , nums[i]);
+            min_sum = Math.min( worst_end , min_sum);
+
+
+        }
+
+        if( max_sum < 0){    // most imp condition for edge case that is ki agar poori ki poori 
+            return max_sum;  // array ki negative numbers ki hui to total sum se minus krne pr
+        }                    // empty aubarrya bachegi which isnt allowed, isliye ye check lgaya
+
+
+        // poori array ka sum nikal lete hai ab 
+        int sum = Arrays.stream(nums).sum();
+
+        res = Math.max( max_sum, sum - min_sum);
+
+
+          
+    return res;
         
-        int totalSum = 0;
-
-        int maxEnding = nums[0], maxSum = nums[0];
-        int minEnding = nums[0], minSum = nums[0];
-
-        for (int num : nums) {
-            totalSum += num;
-        }
-
-        for (int i = 1; i < nums.length; i++) {
-            maxEnding = Math.max(nums[i], maxEnding + nums[i]);
-            maxSum = Math.max(maxSum, maxEnding);
-
-            minEnding = Math.min(nums[i], minEnding + nums[i]);
-            minSum = Math.min(minSum, minEnding);
-        }
-
-        // If all elements are negative, return the normal max subarray sum
-        if (maxSum < 0) {
-            return maxSum;
-        }
-
-        return Math.max(maxSum, totalSum - minSum);
     }
 }
