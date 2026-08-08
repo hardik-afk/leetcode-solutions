ab70667b0ae7d2ef52785c1fb8f323ab6ee6d916
hardik-afk <hardiksharma31082005@gmail.com>
2026-08-09 03:02:14 +0530

    Sync LeetCode solution: Squares of a Sorted Array (Java)

---
+++ Easy/1019_Squares_of_a_Sorted_Array.java
@@ -0,0 +1,40 @@
+/*
+ * Problem: Squares of a Sorted Array
+ * Problem ID: 1019
+ * Difficulty: Easy
+ * Language: Java
+ * Runtime: 1 ms
+ * Memory: 47.5 MB
+ * Synced From: LeetCode
+ * Date: 2026-08-08
+ */
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+        int pos = n - 1;
+
+        while (left <= right) {
+
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[pos] = leftSquare;
+                left++;
+            } else {
+                result[pos] = rightSquare;
+                right--;
+            }
+
+            pos--;
+        }
+
+        return result;
+    }
+}
